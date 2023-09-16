3bfc822662498f8ea1bcf5ca38278de97df364e8
fanboykun <irfanramadhan1812@gmail.com>
2023-09-16 19:51:31 +0700

    change table composition in show loan

--- resources/views/livewire/loan/show-loan.blade.php
+++ resources/views/livewire/loan/show-loan.blade.php
@@ -101,37 +101,15 @@
                     <table class="w-full table-auto text-sm text-left text-gray-500">
                         <thead class="w-full text-xs text-gray-700 uppercase bg-gray-50 sticky top-0 whitespace-pre-line z-10">
                             <tr class="bg-gray-50">
-                                <th scope="col" class="w-6"></th>
                                 <th scope="col" class="px-4 py-3">Nama Nasabah</th>
                                 <th scope="col" class="px-4 py-3 text-right">Jumlah Pembayaran Cicilan</th>
                                 <th scope="col" class="px-4 py-3 text-center">Tanggal Dibayar</th>
+                                <th scope="col" class="px-4 py-3 text-center">Aksi</th>
                             </tr>
                         </thead>
                         <tbody class="min-w-full">
                             @forelse ($loan->installments as $installment)
                                 <tr class="border-b hover:bg-gray-100 items-center even:bg-slate-50">
-                                    <td>
-                                        <x-dropdown align="left" width="48">
-                                            <x-slot name="trigger">
-                                                <button id="dropdownButton" data-dropdown-toggle="dropdown" class="p-1 bg-white shadow-lg text-slate-800 rounded-lg focus:bg-slate-800 focus:text-white active:bg-slate-800 active:text-white" type="button">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
-                                                    </svg>
-                                                </button>
-                                            </x-slot>
-                                            <x-slot name="content" class="shadow-xl bg-indigo-400">
-                                                <button class="border-b border-gray-600 block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
-                                                    Edit Data
-                                                </button>
-                                                <button class="border-b border-gray-600 block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
-                                                    Hapus Data
-                                                </button>
-                                                <x-dropdown-link :href="route('profile.edit')" class="border-b border-gray-600">
-                                                    {{ __('Detail Pinjaman') }}
-                                                </x-dropdown-link>
-                                            </x-slot>
-                                        </x-dropdown>
-                                    </td>
                                     <td class="px-4 py-2 font-medium text-gray-900 break-words">
                                         {{ $installment->loan->customer->name }}
                                         <span class="text-sm text-gray-400 ml-1 block">ID Nasabah : {{ $installment->loan->customer->id }}</span>
@@ -142,6 +120,9 @@
                                     <td class="px-4 py-2 font-medium text-gray-900 text-center break-words">
                                         {{ $installment->created_at->format('H:i-l, d/F/Y') }}
                                         <span class="text-sm text-gray-400 ml-1 block">Oleh : {{ $installment->user->name }}</span>
+                                    </td>
+                                    <td class="justify-center text-center">
+                                        <button type="button" class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded border border-blue-400">Detail</button>
                                     </td>
                                 </tr>
                             @empty

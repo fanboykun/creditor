1657b4c403bd4976787e580f4f899377df77e667
fanboykun <irfanramadhan1812@gmail.com>
2023-09-16 19:49:16 +0700

    add functionality to update installment

--- app/Http/Livewire/Installment/IndexInstallment.php
+++ app/Http/Livewire/Installment/IndexInstallment.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Livewire\Installment;
 
-use App\Models\Customer;
 use Livewire\Component;
 use App\Models\Installment;
+use App\Models\Loan;
 use Illuminate\Support\Facades\DB;
 
 class IndexInstallment extends Component
 {
     public $s;
-    protected $perPage = 10;
+    public $perPage = 10;
     protected $queryString = ['s' => ['except' => '']];
+
+    public $selected_installment;
+
 
     public function render() : \Illuminate\View\View
     {
@@ -22,8 +25,73 @@ class IndexInstallment extends Component
         ->groupBy(function ($item) {
             return $item->created_at->format('Y-m-d');
         });
-        
+
         return view('livewire.installment.index-installment', compact('installments'));
+    }
+
+    public function showInstallment(object|array $installment) : void
+    {
+        $this->selected_installment =  $installment;
+        $this->dispatchBrowserEvent('open-modal', 'show-installment');
+    }
+
+    public function updateInstallment() : void
+    {
+        $loan = (object) $this->selected_installment['loan'];
+        $installment = Installment::findOrFail($this->selected_installment['id']);
+        [$paid, $remaining] = $this->findUpdatedPaidAndRemaining($loan, $this->selected_installment['amount'], $installment);
+        $this->validate([
+            'selected_installment.amount' => 'numeric|required|min:100000|max:'.$loan->remaining.''
+        ], [
+            'selected_installment.amount.required' => 'The amount field is required',
+            'selected_installment.amount.numeric' => 'The amount field must the amount of money to pay',
+            'selected_installment.amount.min' => 'The amount to pay must not lower than Rp 100.000',
+            'selected_installment.amount.max' => 'The amount to pay must not greater than Rp '.number_format($loan->remaining, 0, ',', '.').'',
+        ]);
+        try{
+            DB::transaction(function() use($loan, $installment, $paid, $remaining){
+               $installment->update([
+                'amount' => $this->selected_installment['amount']
+               ]);
+                Loan::where('id', $loan->id)->update([
+                    'paid' => $paid,
+                    'remaining' => $remaining,
+                    'status' => (bool) ($loan->total == $paid),
+                    'updated_at' => now()
+                ]);
+            });
+        }catch(\Exception $e){
+            throw($e);
+        }
+        $this->dispatchBrowserEvent('close-modal');
+    }
+
+    private function findUpdatedPaidAndRemaining(object $loan, float $amount, object $installment) : mixed
+    {
+        if($installment->amount == $amount){
+            $this->dispatchBrowserEvent('close-modal');
+            $this->reset('selected_installment');
+            return null;
+        }elseif( $amount >= $installment->amount){
+            $paid = $loan->paid + ($amount - $installment->amount);
+            $remaining = $loan->remaining - ($amount - $installment->amount);
+            return [$paid, $remaining];
+        }elseif($amount <= $installment->amount){
+            $paid = $loan->paid - ($installment->amount - $amount);
+            $remaining = $loan->remaining + ($installment->amount - $amount);
+            return [$paid, $remaining];
+        }
+    }
+
+    public function deleteInstallment() : void
+    {
+        $this->dispatchBrowserEvent('close-modal');
+        $this->dispatchBrowserEvent('open-modal', 'delete-installment');
+    }
+
+    public function destroyInstallment() : void
+    {
+        $this->dispatchBrowserEvent('close-modal');
     }
 
     public function loadMore() : void
--- resources/views/livewire/installment/index-installment.blade.php
+++ resources/views/livewire/installment/index-installment.blade.php
@@ -59,7 +59,7 @@
                             <tr class="bg-gray-50">
                                 <th scope="col" class="px-4 py-3">Nama Nasabah</th>
                                 <th scope="col" class="px-4 py-3 text-right">Jumlah Pembayaran Cicilan</th>
-                                <th scope="col" class="px-4 py-3 text-center">Tanggal Dibayar</th>
+                                <th scope="col" class="px-4 py-3 text-center">Tanggal Dibuat</th>
                                 <th scope="col" class="px-4 py-3 text-center">Aksi</th>
                             </tr>
                         </thead>
@@ -88,7 +88,7 @@
                                             <span class="text-sm text-gray-400 ml-1 block">Oleh : {{ $installment->user->name }}</span>
                                         </td>
                                         <td class="justify-center text-center">
-                                            <a href="#" class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded border border-blue-400">Detail</a>
+                                            <button type="button" wire:click="showInstallment({{ $installment }})" class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded border border-blue-400">Detail</button>
                                         </td>
                                     </tr>
                                     @empty
@@ -119,4 +119,99 @@
             </div>
         </div>
     </section>
+
+    <x-modal name="show-installment" x-data={} focusable>
+        <div class="px-6 py-6 lg:px-8" @close-modal.window="show = false">
+            @if($selected_installment != null)
+            <h3 class="mb-4 text-xl font-medium text-gray-900 underline"><a href="{{ route('loans.show', $selected_installment['loan']['id']) }}">Detail Data Cicilan ({{ $selected_installment['loan']['id'] }})</a></h3>
+            <form class="space-y-6" wire:submit.prevent="updateInstallment()">
+                <div>
+                    <label for="loan_id" class="block mb-2 text-sm font-medium text-gray-900">ID Peminjaman</label>
+                    <input type="number" value="{{ $selected_installment['loan_id'] }}" disabled name="loan_id" id="loan_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
+                </div>
+                <div>
+                    <label for="amount" class="block mb-2 text-sm font-medium text-gray-900">Jumlah Bayar <span class="text-red-400 text-xs">*</span></label>
+                    <div class="relative mb-2">
+                        <div class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
+                          <span class="text-sm text-gray-700">Rp</span>
+                        </div>
+                        <input type="number" id="amount" wire:model.defer="selected_installment.amount" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5" required>
+                    </div>
+                    <span class="text-gray-400 text-xs mt-1">Sisa cicilan adalah Rp <span class="font-bold text-black text-sm"> {{ number_format($selected_installment['loan']['remaining'], 0, ',', '.') }}</span></span>
+                    @error('selected_installment.amount')
+                        <span class="text-red-400 text-sm block">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="flex justify-between">
+                    <x-secondary-button x-on:click="$dispatch('close')">Cancel</x-secondary-button>
+                    <x-primary-button>Save</x-primary-button>
+                </div>
+                <div class="text-sm font-medium text-gray-500">
+                    <span class="mx-2">Ingin menghapus data?</span><button type="button" wire:click="deleteInstallment()" class="bg-red-100 text-red-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded">Hapus Data</button>
+                </div>
+            </form>
+            @endif
+        </div>
+    </x-modal>
+
+    <x-modal name="delete-installment" x-data={} focusable>
+        <div @close-modal.window="show = false">
+            <form method="post" wire:submit.prevent="destroyInstallment" class="p-6">
+
+                <h2 class="text-lg font-medium text-gray-900">
+                    {{ __('Apakah anda yakin ingin menghapus cicilan ini?') }}
+                </h2>
+
+                <p class="mt-1 text-sm text-gray-600">
+                    {{ __('Ketika data ini dihapus, maka akan mempengaruhi Pinjaman dan data yang telah dihapus tidak akan bisa dikembalikan?') }}
+                </p>
+                @if($selected_installment != null)
+                <div class="flex flex-col gap-y-2  py-2 px-6 border border-indigo-400 rounded-lg">
+                    <div class="flex justify-between py-1.5 bg-gray-50">
+                        <div class="items-end text-end">
+                            Nasabah :
+                        </div>
+                        <div class="items-end">
+                            {{ $selected_installment['loan']['customer']['name'] }}
+                        </div>
+                    </div>
+                    <div class="flex justify-between py-1.5">
+                        <div class="items-end text-end">
+                            Jumlah Pinjaman :
+                        </div>
+                        <div class="items-end">
+                            Rp {{ number_format($selected_installment['loan']['total'], 0, ',', '.' ) }}
+                        </div>
+                    </div>
+                    <div class="flex justify-between py-1.5 bg-gray-50">
+                        <div class="items-end text-end">
+                            Sisa Pinjaman :
+                        </div>
+                        <div class="items-end">
+                            Rp {{ number_format($selected_installment['loan']['remaining'], 0, ',', '.' ) }}
+                        </div>
+                    </div>
+                    <div class="flex justify-between py-1.5">
+                        <div class="items-end text-end">
+                            Terbayar :
+                        </div>
+                        <div class="items-end">
+                            Rp {{ number_format($selected_installment['loan']['paid'], 0, ',', '.' ) }}
+                        </div>
+                    </div>
+                </div>
+                @endif
+
+                <div class="mt-6 flex justify-end">
+                    <x-secondary-button x-on:click="$dispatch('close')">
+                        {{ __('Cancel') }}
+                    </x-secondary-button>
+
+                    <x-danger-button class="ml-3">
+                        {{ __('Delete Installment') }}
+                    </x-danger-button>
+                </div>
+            </form>
+        </div>
+    </x-modal>
 </div>
